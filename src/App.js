50ad04ba9e89c1f779555225e7b8ea0ff97be461
William Wang <w2.wang@sea.samsung.com>
2017-05-22 19:21:51 -0700

    props for name and class

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         </div>
 
         <div className="App-intro">
-          Hello, <Name />
+          Hello, <Name name='William' class='inline'/>
         </div>
 
       </div>
